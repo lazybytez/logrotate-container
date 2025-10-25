af35a461799fb595ce92ca0d0330623c1a479793
Pascal Zarrad <p.zarrad@outlook.de>
2025-10-25 18:24:43 +0200

    feat: add our repository template files

---
+++ commitlint.config.cjs
@@ -0,0 +1,8 @@
+// Configuration of commitlint to check commit message guidelines
+module.exports = {
+    extends: ["@commitlint/config-conventional"],
+    parserPreset: "conventional-changelog-conventionalcommits",
+    rules: {
+        "subject-max-length": [2, "always", 50],
+    }
+};
